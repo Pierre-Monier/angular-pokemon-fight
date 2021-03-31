8997c5b584fee7f50a854e18bbe9d0a0df1c2e3c
PIerre Monier <p.monier96@gmail.com>
2021-03-31 11:18:54 +0200

    merge main

--- src/app/game/game-container/game-container.component.ts
+++ src/app/game/game-container/game-container.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {GameService} from '../../shared/service/game.service';
+import {INIT} from '../../shared/model/game/game-action';
+import {AppUserService} from '../../shared/service/app-user.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-game-container',
@@ -6,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gameService: GameService, private appUserService: AppUserService, public activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams
+      .subscribe((params) => {
+        console.log('params : ', params);
+      });
+
+    // const user = this.appUserService.getCurrentAppUser();
+    // const boss = history.state.data;
+    // if (user && boss) {
+    //   console.log('initing State with user : ', user, ' boss : ', boss);
+    //   this.gameService.updateGame(INIT, {user, boss});
+    // } else {
+    //   console.error('trying to init the game with user : ', user, ' boss : ', boss, window.history.state);
+    // }
   }
 
 }
