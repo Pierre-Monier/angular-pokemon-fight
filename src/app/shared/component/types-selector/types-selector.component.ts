233db95e4d72c0a72aaf638df5573c41196807c1
Nion <mateo.piccarreta@gmail.com>
2021-04-12 16:48:14 +0200

    Loading fait, formulaires en cours

--- src/app/shared/component/types-selector/types-selector.component.ts
+++ src/app/shared/component/types-selector/types-selector.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-types-selector',
   templateUrl: './types-selector.component.html',
-  styleUrls: ['./types-selector.component.css'],
+  styleUrls: ['./types-selector.component.scss'],
 })
 export class TypesSelectorComponent implements OnInit {
   @Input()
